88a05f5a1eb81861525ce3be45f08f05425622bd
fl1k <vgolubovic97@gmail.com>
2022-12-13 14:06:09 +0100

    added files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {AppBar, useScrollTrigger} from "@mui/material";
+import {Box} from "@mui/system";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+    });
+
+    return (
+        <>
+            <AppBar position={trigger ? "fixed" : "absolute"} color={trigger ? "default" : "transparent"}
+                    elevation={0}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        minHeight: "70px",
+                        p: 0
+                    }}
+                    className={"navbarAll"}>
+                <Box sx={{height: "200px", bgcolor: "red"}}></Box>
+            </AppBar>
+
+            <Component {...pageProps} />
+        </>
+    );
 }
